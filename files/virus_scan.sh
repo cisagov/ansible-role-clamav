dbd94b92151ffaa0cdb42b6922a97ebe5d061b4b
Felddy <markf@geekpad.com>
2019-09-06 13:30:17 -0400

    Add option to speed-up scans

--- files/virus_scan.sh
+++ files/virus_scan.sh
@@ -7,7 +7,7 @@ LAST_SCAN_LOG_FILENAME='/var/log/clamav/lastscan.log'
 LAST_DETECTION_FILENAME='/var/log/clamav/last_detection'
 
 # scan the entire system and write to the log
-clamdscan --infected --fdpass / &> ${LAST_SCAN_LOG_FILENAME}
+clamdscan --multiscan --infected --fdpass / &> ${LAST_SCAN_LOG_FILENAME}
 
 # if any infections are found, touch the detection file
 if ! grep -q "^Infected files: 0$" ${LAST_SCAN_LOG_FILENAME}
