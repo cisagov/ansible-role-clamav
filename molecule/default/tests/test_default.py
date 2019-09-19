9d8cb7433b7f0f5c062525e4c6bad5067164cfc8
Jeremy Frasier <jeremy.frasier@trio.dhs.gov>
2019-09-18 23:15:19 -0400

    Add a test to check that the appropriate packages were installed

--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -1,7 +1,7 @@
 """Module containing the tests for the default scenario."""
 
 import os
-import pytest
+# import pytest
 
 import testinfra.utils.ansible_runner
 
@@ -10,7 +10,18 @@ testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
 ).get_hosts("all")
 
 
-@pytest.mark.parametrize("x", [True])
-def test_packages(host, x):
-    """Run a dummy test, just to show what one would look like."""
-    assert x
+def test_pip(host):
+    """Test that the appropriate packages were installed."""
+    if host.system_info.distribution == "fedora":
+        pkgs = ["clamd", "clamav-update"]
+    else:
+        pkgs = ["clamav-daemon"]
+    packages = [host.package(pkg) for pkg in pkgs]
+    installed = [package.is_installed for package in packages]
+    assert len(pkgs) != 0
+    assert all(installed)
+
+# @pytest.mark.parametrize("x", [True])
+# def test_packages(host, x):
+#     """Run a dummy test, just to show what one would look like."""
+#     assert x
