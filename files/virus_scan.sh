0df56f3ca4cd1e2e791b57f665ef2fce7a8d0730
Jeremy Frasier <jeremy.frasier@trio.dhs.gov>
2019-09-19 14:53:33 -0400

    Tidy up virus_scan.sh

--- files/virus_scan.sh
+++ files/virus_scan.sh
@@ -7,7 +7,12 @@ LAST_SCAN_LOG_FILENAME='/var/log/clamav/lastscan.log'
 LAST_DETECTION_FILENAME='/var/log/clamav/last_detection'
 
 # scan the entire system and write to the log
-clamscan --infected --log=${LAST_SCAN_LOG_FILENAME} --recursive --exclude-dir=/dev --exclude-dir=/sys --exclude-dir=/proc /
+clamscan --infected --recursive \
+         --log=${LAST_SCAN_LOG_FILENAME} \
+         --exclude-dir=/dev \
+         --exclude-dir=/sys \
+         --exclude-dir=/proc \
+         /
 
 # if any infections are found, touch the detection file
 if ! grep -q "^Infected files: 0$" ${LAST_SCAN_LOG_FILENAME}
