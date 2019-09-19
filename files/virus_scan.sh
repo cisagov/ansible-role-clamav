e60f5b3dd98104cf3289349b4812a4dd3fd461e3
Jeremy Frasier <jeremy.frasier@trio.dhs.gov>
2019-09-18 23:01:23 -0400

    Modify script to use clamscan instead of clamdscan

--- files/virus_scan.sh
+++ files/virus_scan.sh
@@ -7,7 +7,7 @@ LAST_SCAN_LOG_FILENAME='/var/log/clamav/lastscan.log'
 LAST_DETECTION_FILENAME='/var/log/clamav/last_detection'
 
 # scan the entire system and write to the log
-clamdscan --multiscan --infected --fdpass / &> ${LAST_SCAN_LOG_FILENAME}
+clamscan --infected --log=${LAST_SCAN_LOG_FILENAME} --recursive --exclude-dir=/dev --exclude-dir=/sys --exclude-dir=/proc /
 
 # if any infections are found, touch the detection file
 if ! grep -q "^Infected files: 0$" ${LAST_SCAN_LOG_FILENAME}
