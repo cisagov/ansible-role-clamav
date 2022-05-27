9fc64dd505450c95ad584cabd37f2fe947797e7f
Mathieu Jourdan <m.jourdan@rennesmetropole.fr>
2022-05-27 14:01:08 +0200

    alphabetize clamscan flags

--- templates/virus_scan.tpl.sh
+++ templates/virus_scan.tpl.sh
@@ -9,17 +9,19 @@ LAST_DETECTION_FILENAME='/var/log/clamav/last_detection'
 
 # Scan the entire file system (modulo excluded trees)
 # and write to the log
-clamscan --infected --recursive \
-  --log=${LAST_SCAN_LOG_FILENAME} \
-{% for dir in clamav_scan_exclude_directories %}
-  --exclude-dir={{ dir }} \
-{% endfor %}
-{% if clamav_scan_move %}
-  --move={{ clamav_scan_quarantine_dir }} \
-{% endif %}
+clamscan \
 {% if clamav_scan_copy %}
   --copy={{ clamav_scan_quarantine_dir }} \
 {% endif %}
+{% for dir in clamav_scan_exclude_directories %}
+  --exclude-dir={{ dir }} \
+{% endfor %}
+  --infected \
+  --log=${LAST_SCAN_LOG_FILENAME} \
+{% if clamav_scan_move %}
+  --move={{ clamav_scan_quarantine_dir }} \
+{% endif %}
+  --recursive \
 {% for flag in clamav_scan_extra_flags %}
   {{ flag }} \
 {% endfor %}
