fcaffab673fd8abd56757e0a853eca9c6fd419ad
c.josso <c.josso@rennesmetropole.fr>
2022-10-13 15:08:16 +0200

    [tests] Second commit of the new folder tree

---
+++ molecule/additional_flags/tests/test_additional_flags.py
@@ -0,0 +1,13 @@
+"""Module containing the tests for the additional flags scenario."""
+
+# Standard Python Libraries
+import os
+
+# Third-Party Libraries
+import pytest
+import testinfra.utils.ansible_runner
+
+testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
+    os.environ["MOLECULE_INVENTORY_FILE"]
+).get_hosts("all")
+
