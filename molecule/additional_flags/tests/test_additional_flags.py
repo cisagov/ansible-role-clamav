f9b98befb29df43cf3ee997eb7a9250fdb0e3d1a
Jeremy Frasier <jeremy.frasier@trio.dhs.gov>
2022-10-26 16:25:45 -0400

    Modify test to make it less brittle

--- molecule/additional_flags/tests/test_additional_flags.py
+++ molecule/additional_flags/tests/test_additional_flags.py
@@ -10,39 +10,6 @@ testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
     os.environ["MOLECULE_INVENTORY_FILE"]
 ).get_hosts("all")
 
-string_template_content = """#!/bin/bash
-# Managed by ansible role clamscan
-
-set -o nounset
-set -o pipefail
-
-LAST_SCAN_LOG_FILENAME='/var/log/clamav/lastscan.log'
-LAST_DETECTION_FILENAME='/var/log/clamav/last_detection'
-
-# Scan the entire file system (modulo excluded trees)
-# and write to the log
-clamscan \\
-  --copy=/var/spool/test-clamav \\
-  --exclude-dir=^/dev/ \\
-  --exclude-dir=^/proc/ \\
-  --exclude-dir=^/sys/ \\
-  --exclude-dir=^/var/spool/test-clamav \\
-  --infected \\
-  --log=${LAST_SCAN_LOG_FILENAME} \\
-  --recursive \\
-  --foo bar \\
-  --bar foo \\
-  /
-
-# if any infections are found, touch the detection file
-if ! grep --quiet "^Infected files: 0$" ${LAST_SCAN_LOG_FILENAME}; then
-  touch ${LAST_DETECTION_FILENAME}
-fi
-"""
-
-# Bytify string content
-template_content = bytes(string_template_content, encoding="utf-8")
-
 
 def test_quarantine_folder(host):
     """Test the quarantine folder."""
@@ -51,7 +18,10 @@ def test_quarantine_folder(host):
 
 
 def test_virus_scan_shell(host):
-    """Test the scan shell script existence then content."""
-    assert host.file("/etc/cron.daily/virus_scan").exists
-    shell_content = host.file("/etc/cron.daily/virus_scan").content
-    assert template_content == shell_content
+    """Test the scan shell script exists with the expected content."""
+    f = host.file("/etc/cron.daily/virus_scan")
+    assert f.exists
+    assert f.is_file
+    assert f.contains(r"--exclude-dir=^/var/spool/test-clamav")
+    assert f.contains(r"--foo bar")
+    assert f.contains(r"--bar foo")
