e12bd19b3297a92acb6debaf42746e3af690c4c5
Jeremy Frasier <jeremy.frasier@gwe.cisa.dhs.gov>
2026-05-29 10:32:57 -0400

    Rename a variable to use lowercase/snake case only
    This gets rid of an N806 error from our flake8 pre-commit linter.

--- molecule/modify_config/tests/test_modify_config.py
+++ molecule/modify_config/tests/test_modify_config.py
@@ -39,7 +39,7 @@ def read_configuration_file(host, software_name):
 
 def test_freshclam_conf(host):
     """Test freshclam configuration content."""
-    databaseMirror_list_assertion = [
+    databasemirror_list_assertion = [
         "db.local.clamav.net",
         "database.clamav.net",
         "dummy.localhost",
@@ -51,14 +51,14 @@ def test_freshclam_conf(host):
         words = lines.split(" ")
         if words[0] == "DatabaseMirror":
             """Test DatabaseMirror values"""
-            assert words[1] in databaseMirror_list_assertion
-            databaseMirror_list_assertion.remove(words[1])
+            assert words[1] in databasemirror_list_assertion
+            databasemirror_list_assertion.remove(words[1])
         elif words[0] == "Bytecode":
             # Should not be there
             """Test Bytecode existence"""
             raise AssertionError('Key "Bytecode" should not exist')
 
-    assert len(databaseMirror_list_assertion) == 0
+    assert len(databasemirror_list_assertion) == 0
 
 
 def test_clamd_conf(host):
